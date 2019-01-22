952eea1399f1c12d4683d851433088c7ac31acdb
Camilla <kamilla.stivenson@gmail.com>
2019-01-22 21:02:13 +0100

    Minor improvements to problem 35

--- src/basics/Problem35.java
+++ src/basics/Problem35.java
@@ -6,15 +6,15 @@ public class Problem35 {
   public static void main(String []args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Input the number of sides of the polygon: ");
-    double sides = sc.nextDouble();
+    int sides = sc.nextInt();
     System.out.print("Input the length of on of the sides: ");
     double length = sc.nextDouble();
-    System.out.print("The area of the polygon with " + sides + " sides is: " + area(sides, length));
+    System.out.format("The area of the polygon with %d sides is: %.2f", sides, area(sides, length));
 
     sc.close();
   }
 
-  public static double area (double sides, double length) {
+  public static double area (int sides, double length) {
     return (sides * Math.pow(length, 2))/(4 * Math.tan(Math.PI / sides));
   }
 }
