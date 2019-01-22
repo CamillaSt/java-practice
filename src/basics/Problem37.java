e6f45dde5652fdff407418e3adfe463e4a969391
camillast <kamilla.stivenson@gmail.com>
2019-01-22 21:38:16 +0100

    Minor fixes to problem 37

--- src/basics/Problem37.java
+++ src/basics/Problem37.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 public class Problem37 {
   public static void main(String []args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a word or a setnence:");
+    System.out.print("Enter a word or a sentence: ");
     String name = sc.nextLine();
-    System.out.print("The reveresed version of your input is: ");
-    reverse(name);
+    System.out.print("The reveresed version of your input is: " + reverse(name));
 
     sc.close();
   }
 
-  public static void reverse (String name) {
-    
-    StringBuilder reverseName = new StringBuilder();
-    reverseName.append(name);
-    reverseName = reverseName.reverse();
-    System.out.print(reverseName);
+  public static String reverse(String name) {
+    StringBuilder reverseName = new StringBuilder(name);
+    return reverseName.reverse().toString();
   }
 }
