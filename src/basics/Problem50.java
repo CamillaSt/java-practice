de7e67922c93f3457d94706e28667a9c0d3361b1
camillast <kamillastivenson@gmail.com>
2019-01-23 22:48:24 +0100

    Add solution for problem 50

--- src/basics/Problem50.java
+++ src/basics/Problem50.java
@@ -6,14 +6,21 @@ public class Problem50 {
     System.out.println("Divisible by 3: ");
     for (int i = 1; i < 100; i++) {
       if (i % 3 == 0) {
-      System.out.print(i + " ");
+        System.out.print(i + " ");
       } 
     }
 
     System.out.println("\n\nDivisible by 5: ");
     for (int j = 1; j < 100; j++) {
       if (j % 5 == 0) {
-      System.out.print(j + " ");
+        System.out.print(j + " ");
+      }
+    }
+
+    System.out.println("\n\nDivisible by both 3 and 5: ");
+    for (int k = 1; k < 100; k++) {
+      if (k % 3 == 0 && k % 5 == 0) {
+        System.out.print(k + " ");
       }
     }
   }
