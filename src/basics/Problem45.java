ee90e0b619014c01af82ad5bb438579e40bd6854
camillast <kamillastivenson@gmail.com>
2019-01-23 14:20:18 +0100

    Change from print to format

--- src/basics/Problem45.java
+++ src/basics/Problem45.java
@@ -7,12 +7,13 @@ public class Problem45 {
   public static void main(String[] args) throws Exception {
     File file = new File("C:\\Users\\Camilla\\Desktop\\names.txt");
     Scanner sc =  new Scanner(file);
+
     System.out.print("This file contains following text: ");
     while (sc.hasNextLine()) {
        System.out.print(sc.nextLine() + " ");
     }
 
     sc.close();
-    System.out.print("\nThe size of this file is: " + file.length() + " bytes");
+    System.out.format("\nThe size of this file is: %d bytes", file.length());
   }
 }
