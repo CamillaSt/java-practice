c515cc80f67e4a042cd3b7b9a094d86c092e1d0e
camillast <kamillastivenson@gmail.com>
2019-01-23 12:20:15 +0100

    Initial solution for problem 44

---
+++ src/basics/Problem44.java
@@ -0,0 +1,14 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Problem44 {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");  
+    int input = sc.nextInt();
+    char a, b, c;
+    System.out.printf("%d + %d%d + %d%d%d\n", input, input, input, input, input, input);
+  }
+}
+
