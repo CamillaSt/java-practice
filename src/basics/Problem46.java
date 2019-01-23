8cdb0b1215e1fe9db6f4ce14aaf76c57ccb03620
camillast <kamillastivenson@gmail.com>
2019-01-23 14:28:23 +0100

    Add solution for problem 46

---
+++ src/basics/Problem46.java
@@ -0,0 +1,11 @@
+package basics;
+
+import java.util.Date;
+
+public class Problem46 {
+  public static void main(String[] args) {
+    Date date = new Date();
+    System.out.print("Current date is: " + date.toString());
+  }
+}
+
