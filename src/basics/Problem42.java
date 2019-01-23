b162ece57700c9338a9fa088def5559b666305c3
camillast <kamillastivenson@gmail.com>
2019-01-23 13:23:44 +0100

    Remove comments as suggested

--- src/basics/Problem42.java
+++ src/basics/Problem42.java
@@ -6,14 +6,11 @@ import java.io.Console;
 public class Problem42 {
   
   public static void main(String []args) {
-    //creating a reference to the Console class
     Console con = System.console();
 
-    System.out.print("Enter your password: ");
-    //saving the input as a Char in char array
+    System.out.print("Enter your password: ");y
     char[] password = con.readPassword();
-    
-    //converting char array to String
+
     String psw = String.valueOf(password);
     System.out.print("Password is: " + psw);
   }
