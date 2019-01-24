f4a56e70b8cf10f6a2336e346939ad27a5b59262
camillast <kamillastivenson@gmail.com>
2019-01-24 13:51:33 +0100

    Change the test method

--- src/basics/Problem52.java
+++ src/basics/Problem52.java
@@ -12,16 +12,13 @@ public class Problem52 {
     int b = sc.nextInt();
     System.out.print("Enter the third number: ");
     int c = sc.nextInt();
-    test(a,b,c);
+    System.out.println("The result is: " + test(a,b,c));
 
     sc.close();
   }
 
-  public static void test(int a, int b, int c){
-    if (a + b == c) {
-      System.out.println("The result is: True");
-    } else {
-      System.out.println("The result is : False");
-    }
+  public static boolean test(int a, int b, int c){
+    return ((a + b) == c || (b + c) == a || (c + a) == b);
   }
  }
+ 
