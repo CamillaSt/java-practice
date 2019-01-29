8077f1cf136c3ddb3d2fc20c6a7e6786e2b45792
camillast <kamillastivenson@gmail.com>
2019-01-29 12:31:48 +0100

    Minor changes

--- src/basics/Problem54.java
+++ src/basics/Problem54.java
@@ -3,15 +3,14 @@ package basics;
 import java.util.Scanner;
 
 public class Problem54 {
-
   public static void main(String[] args) {
-    System.out.println("Im thinking of a number between 0 and 10");
+    System.out.println("I'm thinking of a number between 0 and 10");
     System.out.println("Your job is to guess that number");   
     long randomNumber = Math.round(Math.random() * 10);
     boolean win = false;
     Scanner sc = new Scanner(System.in);
 
-    while (win == false) {
+    while (!win) {
       System.out.print("Enter the number: ");
       int guess = sc.nextInt();
       if (guess == randomNumber) {
