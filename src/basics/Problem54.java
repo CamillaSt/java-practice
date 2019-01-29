0cc43b3f5dd068126c53142f30ac2d9cc5385171
camillast <kamillastivenson@gmail.com>
2019-01-29 12:08:24 +0100

    Solution for problem 54

---
+++ src/basics/Problem54.java
@@ -0,0 +1,28 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Problem54 {
+
+  public static void main(String[] args) {
+    System.out.println("Im thinking of a number between 0 and 10");
+    System.out.println("Your job is to guess that number");   
+    long randomNumber = Math.round(Math.random() * 10);
+    boolean win = false;
+    Scanner sc = new Scanner(System.in);
+
+    while (win == false) {
+      System.out.print("Enter the number: ");
+      int guess = sc.nextInt();
+      if (guess == randomNumber) {
+        win = true;
+      } else if (guess > randomNumber) {
+        System.out.println("The secret number is lower");
+      } else if (guess < randomNumber) {
+        System.out.println("The secret number is higher");
+      }
+    }
+    System.out.print("You win!");
+    sc.close();
+  }
+}
